483ddc75ecaef82e2d13127fedab376d6dc7c943
zhujiancc <850232649@qq.com>
2022-02-10 17:10:18 +0800

    add loth.es

---
+++ loth.es/ElasticSearchOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace loth.es
+{
+    public class ElasticSearchOptions
+    {
+        /// <summary>
+        /// node 地址
+        /// </summary>
+        [Required]
+        public string Uris { get; set; }
+
+        /// <summary>
+        /// 请求超时时间/s
+        /// </summary>
+        public int RequestTimeout { get; set; } = 20;
+
+        /// <summary>
+        /// Ping超时时间/s
+        /// </summary>
+        public int PingTimeout { get; set; } = 20;
+    }
+}
---
+++ loth.es/ElasticSearchServiceCollectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Elasticsearch.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loth.es
+{
+    public static class ElasticSearchServiceCollectionExtensions
+    {
+        public static IServiceCollection AddElasticSearch(this IServiceCollection services, Action<ElasticSearchOptions> setupAction)
+        {
+            if (setupAction == null)
+            {
+                return services;
+            }
+
+            var options = new ElasticSearchOptions();
+
+            setupAction(options);
+
+            var uris = options.Uris.Split(",").Select(x => new Uri(x));
+
+            var logger = services.BuildServiceProvider().GetService<ILogger>();
+
+            var pool = new StaticConnectionPool(uris);
+            var settings = new ConnectionSettings(pool);
+            settings
+                .RequestTimeout(TimeSpan.FromSeconds(options.RequestTimeout))
+                .PingTimeout(TimeSpan.FromSeconds(options.PingTimeout))
+                .OnRequestCompleted(apiCallDetails =>
+                {
+                    var list = new List<string>();
+                    // log out the request and the request body, if one exists for the type of request
+                    if (apiCallDetails.RequestBodyInBytes != null)
+                    {
+                        list.Add(
+                            $"{apiCallDetails.HttpMethod} {apiCallDetails.Uri} " +
+                            $"{Encoding.UTF8.GetString(apiCallDetails.RequestBodyInBytes)}");
+                    }
+                    else
+                    {
+                        list.Add($"{apiCallDetails.HttpMethod} {apiCallDetails.Uri}");
+                    }
+                    logger.LogDebug(string.Join(";", list));
+                });
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton<IElasticClient>(client);
+
+            return services;
+        }
+
+    }
+}
